510e6ad767c44826fb14d3a767f51a729e702bb4
drv-agwl <drv.agwl@gmail.com>
2025-05-28 14:33:45 -0700

    Final commit for the 4D MitoSpace paper😊

---
+++ autoencoder/models.py
@@ -0,0 +1,242 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+class MitoSpace3DEncoder(nn.Module):
+    def __init__(self, input_dim=2):
+        super(MitoSpace3DEncoder, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv3d(in_channels = input_dim,
+                      out_channels = 4,
+                      kernel_size = (3, 3, 3),
+                      stride = (2, 2, 2),
+                      padding = (1, 1, 1),
+                      bias = True),
+            nn.ReLU()
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv3d(in_channels = 4,
+                      out_channels = 8,
+                      kernel_size = (3, 3, 3),
+                      stride = (1, 1, 1),
+                      padding = (1, 1, 1),
+                      bias = True),
+            nn.ReLU()
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv3d(in_channels = 8,
+                      out_channels = 8,
+                      kernel_size = (3, 3, 3),
+                      stride = (1, 1, 1),
+                      padding = (1, 1, 1),
+                      bias = True),
+            nn.ReLU()
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv3d(in_channels = 8,
+                      out_channels = 64,
+                      kernel_size = (3, 3, 3),
+                      stride = (2, 2, 2),
+                      padding = (1, 1, 1),
+                      bias = True),
+            nn.ReLU()
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv3d(in_channels = 64,
+                               out_channels = 128,
+                               kernel_size = (3, 3, 3),
+                               stride = (1, 1, 1),
+                               padding = (1, 1, 1),
+                               bias=True),
+            nn.ReLU()
+        )
+
+        self.conv6 = nn.Sequential(
+            nn.Conv3d(in_channels=128,
+                      out_channels=64,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.conv7 = nn.Sequential(
+            nn.Conv3d(in_channels=64,
+                      out_channels=64,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.conv8 = nn.Sequential(
+            nn.Conv3d(in_channels=64,
+                      out_channels=2,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        assert(len(x.shape) == 6)
+
+        batch_size, timesteps, channels, stacks, height, width = x.shape
+        x = x.view(batch_size * timesteps, channels, stacks, height, width)
+
+        noisy_x = torch.nn.functional.interpolate(x,
+                                                  scale_factor=(0.25, 0.25, 0.25),
+                                                  mode='nearest',
+                                                  align_corners=None,
+                                                )
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+
+        _, _, new_stacks, new_height, new_width = x.shape
+        noisy_x = noisy_x.view(batch_size, timesteps, channels, new_stacks, new_height, new_width)
+        x = x.view(batch_size, timesteps, channels, new_stacks, new_height, new_width)
+
+        return torch.cat((x, noisy_x), dim = 2)
+
+class MitoSpace3DDecoder(nn.Module):
+    def __init__(self, output_dim=2):
+        super(MitoSpace3DDecoder, self).__init__()
+
+        self.deconv1 = nn.Sequential(
+            nn.Upsample(scale_factor=(1, 1, 1), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=4,
+                      out_channels=64,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.deconv2 = nn.Sequential(
+            nn.Upsample(scale_factor=(1, 1, 1), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=64,
+                      out_channels=64,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.deconv3 = nn.Sequential(
+            nn.Upsample(scale_factor=(1, 1, 1), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=64,
+                      out_channels=128,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.deconv4 = nn.Sequential(
+            nn.Upsample(scale_factor=(1, 1, 1), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=128,
+                      out_channels=64,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.deconv5 = nn.Sequential(
+            nn.Upsample(scale_factor=(2, 2, 2), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=64,
+                      out_channels=8,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.deconv6 = nn.Sequential(
+            nn.Upsample(scale_factor=(1, 1, 1), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=8,
+                      out_channels=8,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.deconv7 = nn.Sequential(
+            nn.Upsample(scale_factor=(1, 1, 1), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=8,
+                      out_channels=4,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+        self.deconv8 = nn.Sequential(
+            nn.Upsample(scale_factor=(2, 2, 2), mode='trilinear', align_corners=True),
+            nn.Conv3d(in_channels=4,
+                      out_channels=output_dim,
+                      kernel_size=(3, 3, 3),
+                      stride=(1, 1, 1),
+                      padding=(1, 1, 1),
+                      bias=True),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        assert(len(x.shape) == 6)
+
+        batch_size, timesteps = x.shape[:2]
+
+        x = x.view(batch_size*timesteps, 4, 15, 64, 64)
+
+        x = self.deconv1(x)
+        x = self.deconv2(x)
+        x = self.deconv3(x)
+        x = self.deconv4(x)
+        x = self.deconv5(x)
+        x = self.deconv6(x)
+        x = self.deconv7(x)
+        x = self.deconv8(x)
+
+        _, _, new_stacks, new_height, new_width = x.shape
+        x = x.view(batch_size, timesteps, 2, new_stacks, new_height, new_width)
+
+        return x
+
+class MitoSpace3DAutoencoder(nn.Module):
+    def __init__(self):
+        super(MitoSpace3DAutoencoder, self).__init__()
+        self.encoder = MitoSpace3DEncoder()
+        self.decoder = MitoSpace3DDecoder()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        return self.decoder(x)
+
+if __name__ == '__main__':
+    batch_size = 1
+    autoencoder = MitoSpace3DAutoencoder().cuda()
+    summary(autoencoder, input_size=(10, 2, 60, 256, 256), batch_size=batch_size)
---
+++ autoencoder/train_model.py
@@ -0,0 +1,76 @@
+import argparse
+import time
+
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from pytorch_lightning.callbacks import ModelCheckpoint
+from torch.utils.data import DataLoader
+from torchvision import models
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import LearningRateMonitor
+from typing import Any, Dict, List, Tuple
+from resnet_model import MitoSpace3ResNetAutoEncoder
+from models import MitoSpace3DAutoencoder
+from model_params import MitoSpace3DAutoencoderHigherParams
+from autoencoder import AutoEncoderRunner
+from autoencoder_dataset import MitoSpaceAutoEncoderDataset
+
+print("Imported packages")
+
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+
+def train_model(model, train_loader):
+    trainer = pl.Trainer(
+        default_root_dir='lightning_logs/sanity_check',
+        accelerator="gpu",
+        devices=-1,
+        max_epochs=100,
+        callbacks=[
+            ModelCheckpoint(
+                save_top_k=3,  # Keep only the best checkpoint
+                monitor="Train/total_loss",  # Monitor cumulative training loss
+                mode="min",
+                save_last=True,  # Minimize cumulative training loss
+            ),
+            LearningRateMonitor("epoch"),
+        ],
+        precision=16,
+    )
+
+    trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
+    trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
+    trainer.fit(model, train_loader)
+
+    result = trainer.test(model, dataloaders=train_loader, verbose=False)
+    return model, result
+
+
+if __name__ == '__main__':
+    print("Started Training Loop")
+    dataset = MitoSpaceAutoEncoderDataset(
+        root_dir='/home/dhruvagarwal/projects/MitoSpace4D/data/2024_subdata/processed_data/')
+    print("Total samples in dataset:", len(dataset))
+
+    # checkpoint_path = "/home/dhruvagarwal/projects/MitoSpace4D/autoencoder/runs/autoencoder/lightning_logs/version_2/checkpoints/epoch=31-step=43136.ckpt"
+    # checkpoint = torch.load(checkpoint_path)
+    # print(checkpoint)
+
+    # Create DataLoader for training
+    train_loader = DataLoader(dataset, batch_size=1,
+                              shuffle=True,
+                              drop_last=True,
+                              num_workers=4,
+                              pin_memory=True,
+                              prefetch_factor=2,
+                              persistent_workers=True
+                              )
+
+    model = MitoSpace3DAutoencoder()
+    runner = AutoEncoderRunner(model=model)
+    print(model)
+    train_model(runner, train_loader)
